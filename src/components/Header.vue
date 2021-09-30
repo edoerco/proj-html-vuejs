4ffffd561a68848be73777f33d58840a04003a14
Edoardo <ercolino.edoardo@gmail.com>
2021-09-30 18:56:33 +0200

    prima parte di header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -18,8 +18,29 @@
 
     <!-- header bottom  -->
     <div class="header-bottom">
-      <div class="container-bottom">
-
+      <div class="overlay">
+        <div class="container-bottom">
+          <div class="info">
+            <div class="nexgen">
+              <div class="nex">
+                <span>NEX</span>
+              </div>
+              <div class="gen">
+                <span>GEN</span>
+              </div>
+            </div>
+            <div class="nav">
+                <li><a href="#">HOME</a></li>
+                <li><a href="#">ABOUT</a></li>
+                <li><a href="#">SERVICES</a></li>
+                <li><a href="#">PROCESS</a></li>
+                <li><a href="#">TESTIMONIALS</a></li>
+                <li><a href="#"><i class="far fa-user"></i></a></li>
+                <li><div class="btn">GET IN TOUCH</div></li>
+            </div>
+          </div>
+          <!-- creare il secondo div contenente il titolo i paragrafi e i due bottoni  -->
+        </div>
       </div>
     </div>
   </header>
@@ -48,14 +69,14 @@ export default {
     display: flex;
     align-items: center;
     justify-content: space-between;
+    font-weight: 200;
+    font-size: 15px;
 
     .socials {
       display: flex;
       justify-content: space-between;
       width: 40%;
       list-style: none;
-      font-weight: 200;
-      font-size: 15px;
 
       .fab {
         color: $silver-sand;
@@ -70,10 +91,72 @@ export default {
   height: 850px;
   background-image: url('../assets/img/bg-9.jpg');
 
-  .container-top {
+  .overlay {
+    height: 100%;
+    width: 100%;
+    background-color: rgba(0,0,0,0.5);
+  }
+
+  .container-bottom {
     width: 60%;
     height: 100%;
     margin: 0 auto;
+    padding-top: 30px;
+
+    .info {
+      display: flex;
+      align-items: center;
+
+      .nexgen {
+      display: flex;
+      align-items: center;
+      font-weight: 500;
+      font-size: 20px;
+
+        .nex {
+          display: flex;
+          justify-content: flex-end;
+          background-color: rgba(03,83,84, 0.3);
+          color: $blue-lagoon;
+          padding: 10px 0 10px 0;
+          width: 90px;
+          letter-spacing: 5px;
+          border-radius: 20px 0 0px 20px;
+        }
+
+        .gen {
+          color: $silver-sand;
+          letter-spacing: 5px;
+        }
+      }
+
+      .nav {
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+        list-style: none;
+        width: 95%;
+        font-weight: 500;
+
+        li a {
+          margin: 0 20px;
+          text-decoration: none;
+          color: $white;
+
+        }
+
+        .btn {
+          padding: 15px 25px;
+          color: $white;
+          font-weight: 500;
+          background-color: $blue-lagoon;
+          border-radius: 5px;
+          letter-spacing: 2px;
+        }
+      }
+    }
+
+
   }
 }
 
