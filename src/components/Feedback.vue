afca176ca969958aec8a29362ced3bcc57de1ba0
Edoardo <ercolino.edoardo@gmail.com>
2021-10-03 18:53:30 +0200

    Pushatina della domenica, feedback concluso

--- src/components/Feedback.vue
+++ src/components/Feedback.vue
@@ -1,7 +1,24 @@
 <template>
   <div class="body">
     <div class="container">
-
+      <div class="container-top">
+        <span>CUSTOMER TESTIMONIALS</span>
+        <div class="title-section">
+          <h2>Trusted</h2>
+          <div class="feedback">
+            <h2>Feedback</h2>
+          </div>
+        </div>
+        <p>We work intensively in search of ideals that can add up in the lives of our customers. This is what moves us and we are grateful for the recognition.</p>
+      </div>
+      <div class="container-bottom">
+        <div class="card" v-for="(item, index) in feedbacks" :key="index">
+        <img :src="require(`../assets/${item.photo}`)" :alt="item.name">
+        <p>Praesent volutpat justo sit amet elementum malesuada. Present sagittis augue justo, in accumsan orci rhoncus at pellentesque.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+        <div class="apex">"</div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -9,6 +26,24 @@
 <script>
 export default {
   name: 'Feedback',
+  data() {
+    return {
+      feedbacks: [
+        {
+          photo: 'img/logo-1.png',
+          name: 'woo commerce'
+        },
+        {
+          photo: 'img/logo-2.png',
+          name: 'wordpress'
+        },
+        {
+          photo: 'img/logo-3.png',
+          name: 'pingdom'
+        },
+      ]
+    }
+  }
 }
 </script>
 
@@ -21,13 +56,92 @@ export default {
   width: 100%;
   height: 800px;
   background-color: $woodsmoke;
+  padding: 150px 0;
 
   .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
     width: 60%;
     height: 100%;
     margin: 0 auto;
-    background-color: rgb(173, 119, 119);
+
+    .container-top {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 75%;
+    margin: 0 auto;
+    text-align: center;
+
+      span {
+        color: $blue-chill;
+        font-size: 15px;
+        font-weight: bold;
+      }
+
+      .title-section {
+        display: flex;
+        align-items: center;
+        font-size: 30px;
+        margin: 20px 0;
+        color: $white;
+
+        .feedback {
+          display: flex;
+          align-items: center;
+          background-color: rgba(03,83,84, 0.5);
+          padding: 10px 15px;
+          border-radius: 5px;
+          margin-left: 10px;
+        }
+      }
+    }
+
+    .container-bottom {
+      display: flex;
+      justify-content: space-between;
+
+      .card {
+        position: relative;
+        width: calc(100% / 3 - 40px);
+        background-color: $shark;
+        padding: 40px;
+        border-radius: 15px;
+
+        &:nth-child(2) {
+          margin: 0 30px;
+        }
+
+        p {             // non mi prende il first-child
+          margin: 10px 0  5px 0;
+          line-height: 22px;
+        }
+
+        img {
+          width: 120px;
+          filter: invert(100%); // vi odio tanto <3
+        }
+
+        .apex {
+          position: absolute;
+          font-size: 100px;
+          color: $mid-gray;
+          right: 30px;
+          bottom: -50px;
+        }
+
+      }
+    }
   }
+}
+
+p {
+  font-weight: 300;
+  font-size: 17px;
+  color: $silver-sand;
 }
 
 </style>
--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,7 +1,22 @@
 <template>
   <div class="body">
     <div class="container">
+      <div class="title">
+        <span>WHAT ARE WE DOING</span>
+        <div class="title-section">
+          <div class="results">
+            <h2>Results</h2>
+          </div>
+            <h2>in Numbers</h2>
+        </div>
+      </div>
 
+      <div class="container-number">
+        <div class="number" v-for="(result, index) in results" :key="index">
+          <span>{{result.number}}</span>
+          <p>{{result.text}}</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -9,6 +24,28 @@
 <script>
 export default {
   name: 'Results',
+  data() {
+    return {
+      results: [
+        {
+          number: 128,
+          text: 'Certifications'
+        },
+        {
+          number: 230,
+          text: 'Employees'
+        },
+        {
+          number: 517,
+          text: 'Customers'
+        },
+        {
+          number: 94,
+          text: 'Countries Served'
+        },
+      ]
+    }
+  }
 }
 </script>
 
@@ -21,12 +58,69 @@ export default {
   height: 600px;
   background-image: url('../assets/img/bg-10.jpg');
   background-position: center;
+  padding: 150px 0;
 
   .container {
     width: 60%;
     height: 100%;
     margin: 0 auto;
-    background-color: #fff; // rimuovere il bc
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+
+    .title {
+      text-align: center;
+    }
+
+    .title span {
+      color: $blue-chill;
+      font-size: 15px;
+      font-weight: bold;
+    }
+
+    .title-section {
+      display: flex;
+      align-items: center;
+      font-size: 30px;
+      color: $white;
+      margin-top: 15px;
+
+
+      .results {
+        display: flex;
+        align-items: center;
+        background-color: rgba(03,83,84, 0.2);
+        padding: 10px 15px;
+        border-radius: 5px;
+        margin-right: 10px;
+      }
+    }
+
+    .container-number {
+      width: 100%;
+      display: flex;
+      justify-content: space-around;
+
+      .number {
+      display: flex;
+      flex-direction: column;
+      text-align: center;
+
+        span {
+          color: $blue-chill;
+          font-size: 45px;
+          font-weight: bold;
+          margin-bottom: 15px;
+        }
+
+        p {
+          color: $white;
+          font-size: 25px;
+          font-weight: bold;
+        }
+      }
+    }
   }
 }
 </style>
